611c4ca09e5b89b52bc8c0517420c195cb40e146
Alan Hardman <alanaktion@gmail.com>
2021-08-10 11:30:25 -0600

    Redirect home to dashboard when logged in

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->middleware('guest');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
